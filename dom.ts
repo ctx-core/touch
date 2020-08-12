93b11790dfbec0405ba3b7a9f8b32601b99038c1
Brian Takita <brian.takita@gmail.com>
2020-08-11 21:16:35 -0700

    - @ctx-core/logger

--- dom.ts
+++ dom.ts
@@ -1,15 +1,11 @@
 import { add__class } from '@ctx-core/dom'
-import { log } from '@ctx-core/logger'
-const logPrefix = '@ctx-core/touch/dom.js'
 export function mount__no_touch() {
-	log(`${logPrefix}|mount__no_touch`)
 	// modernizer-like touch workaround
 	if (!('ontouchstart' in document.documentElement)) {
 		add__class(document.documentElement, 'no-touch')
 	}
 }
 export function enable__scrolling__touch(ctx) {
-	log(`${logPrefix}|enable__scrolling__touch`)
 	let { scrolling__touch } = ctx
 	if (!scrolling__touch) {
 		document.removeEventListener('touchmove',
@@ -19,7 +15,6 @@ export function enable__scrolling__touch(ctx) {
 	}
 }
 export function disable__scrolling__touch(ctx) {
-	log(`${logPrefix}|disable__scrolling__touch`)
 	let { scrolling__touch } = ctx
 	if (
 		scrolling__touch == null
@@ -32,10 +27,8 @@ export function disable__scrolling__touch(ctx) {
 	}
 }
 function ontouchmove__enable() {
-	log(`${logPrefix}|ontouchmove__enable`)
 	return true
 }
 function ontouchmove__disable(e) {
-	log(`${logPrefix}|ontouchmove__disable`)
 	e.preventDefault()
 }
